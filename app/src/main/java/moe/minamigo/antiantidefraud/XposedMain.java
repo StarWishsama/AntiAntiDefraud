4b4b68cce2655535b33f3337c83625769a1b4884
StarWishsama <starwishsama@outlook.com>
2023-04-21 22:41:57 +0800

    update for 1.5.1-20230217.0

--- app/src/main/java/moe/minamigo/antiantidefraud/XposedMain.java
+++ app/src/main/java/moe/minamigo/antiantidefraud/XposedMain.java
@@ -1,11 +1,8 @@
 package moe.minamigo.antiantidefraud;
 
-import org.json.JSONArray;
-import org.json.JSONObject;
-
 import java.lang.reflect.Field;
 import java.lang.reflect.Method;
-import java.util.List;
+import java.util.ArrayList;
 
 import de.robv.android.xposed.IXposedHookLoadPackage;
 import de.robv.android.xposed.XC_MethodHook;
@@ -24,19 +21,31 @@ public class XposedMain implements IXposedHookLoadPackage {
             final Class<?> guardApplication = XposedHelpers.findClass("com.miui.guardprovider.GuardApplication", lpparam.classLoader);
             if (guardApplication != null) {
                 Field[] guardApplicationFields = guardApplication.getDeclaredFields();
+                boolean debugMode = false;
+
                 for (Field field : guardApplicationFields) {
-                    if (field.getName().equals("c")) {
-                        XposedHelpers.setStaticBooleanField(guardApplication, "c", true);
-                        XposedBridge.log("[[[AntiAntiDefraud]]] Info: GuardProvider will work as debug mode!");
+                    if ("b".equals(field.getName())) {
+                        try {
+                            field.setAccessible(true);
+                            field.set(guardApplication, true);
+                            XposedBridge.log("[[[AntiAntiDefraud]]] Info: GuardProvider will work as debug mode!");
+                            debugMode = true;
+                        } catch (Throwable e) {
+                            XposedBridge.log("[[[AntiAntiDefraud]]] Warn: Unable to enable debug mode!");
+                            e.printStackTrace();
+                        }
                     }
+                }
+
+                if (!debugMode) {
                     XposedBridge.log("[[[AntiAntiDefraud]]] Warning: GuardProvider debug mode flag not found!");
                 }
-            }else{
+            } else {
                 XposedBridge.log("[[[AntiAntiDefraud]]] Warning: GuardApplication class not found. GuardProvider will not work as debug mode! ");
             }
 
             // Prevent miui from uploading app list
-            final Class<?> antiDefraudAppManager = XposedHelpers.findClassIfExists("com.miui.guardprovider.engine.mi.antidefraud.AntiDefraudAppManager", lpparam.classLoader);
+            final Class<?> antiDefraudAppManager = XposedHelpers.findClassIfExists("p.c", lpparam.classLoader);
             if (antiDefraudAppManager == null) {
                 XposedBridge.log("[[[AntiAntiDefraud]]] Skip: AntiDefraudAppManager class not found.");
                 return;
@@ -46,12 +55,25 @@ public class XposedMain implements IXposedHookLoadPackage {
 
             final Method[] methods = antiDefraudAppManager.getDeclaredMethods();
             Method getAllUnSystemAppsStatus = null;
+            Method getAllDetectUnsafeApps = null;
+            Method getUnSystemAppList = null;
             for (Method method : methods) {
-                if (method.getName().equals("getAllUnSystemAppsStatus") && method.getParameterTypes().length == 1) {
+                // getAllUnSystemAppsStatus
+                if (method.getName().equals("b") && method.getParameterTypes().length == 1) {
                     getAllUnSystemAppsStatus = method;
-                    break;
+                }
+
+                // getAllDetectUnsafeApps
+                if ("a".equals(method.getName()) && method.getParameterTypes().length == 0) {
+                    getAllDetectUnsafeApps = method;
+                }
+
+                // getUnSystemAppList
+                if ("g".equals(method.getName()) && method.getParameterTypes().length == 1) {
+                    getUnSystemAppList = method;
                 }
             }
+
             if (getAllUnSystemAppsStatus == null) {
                 XposedBridge.log("[[[AntiAntiDefraud]]] Skip: getAllUnSystemAppsStatus method not found.");
                 return;
@@ -59,86 +81,41 @@ public class XposedMain implements IXposedHookLoadPackage {
                 XposedBridge.log("[[[AntiAntiDefraud]]] Info: getAllUnSystemAppsStatus method found.");
             }
 
+            if (getAllDetectUnsafeApps == null) {
+                XposedBridge.log("[[[AntiAntiDefraud]]] Skip: getAllDetectUnsafeApps method not found.");
+                return;
+            } else {
+                XposedBridge.log("[[[AntiAntiDefraud]]] Info: getAllDetectUnsafeApps method found.");
+            }
+
+            if (getUnSystemAppList == null) {
+                XposedBridge.log("[[[AntiAntiDefraud]]] Skip: getUnSystemAppList method not found.");
+                return;
+            } else {
+                XposedBridge.log("[[[AntiAntiDefraud]]] Info: getUnSystemAppList method found.");
+            }
+
             XposedBridge.hookMethod(getAllUnSystemAppsStatus, new XC_MethodHook() {
                 @Override
-                protected void beforeHookedMethod(MethodHookParam methodHookParam) throws Throwable {
-                    List<Object> list = (List) methodHookParam.args[0];
-
-                    String MIUI_VERSION = null;
-                    Field[] antiDefraudAppManagerFields = antiDefraudAppManager.getDeclaredFields();
-                    for (Field field : antiDefraudAppManagerFields) {
-                        if (field.getName().equals("MIUI_VERSION")) {
-                            MIUI_VERSION = (String) XposedHelpers.getStaticObjectField(antiDefraudAppManager, "MIUI_VERSION");
-                        }
-                    }
-                    if (MIUI_VERSION == null) {
-                        XposedBridge.log("[[[AntiAntiDefraud]]] Warning: Can't get MIUI_VERSION.");
-                    }
-
-                    String uuid = null;
-                    final Class<?> uuidHelper = XposedHelpers.findClassIfExists("i.b", lpparam.classLoader);
-                    if(uuidHelper != null){
-                        final Method[] uuidHelperMethods = uuidHelper.getDeclaredMethods();
-                        Method getUUID = null;
-                        for (Method method : uuidHelperMethods) {
-                            if (method.getName().equals("b") && method.getParameterTypes().length == 0) {
-                                getUUID = method;
-                                break;
-                            }
-                        }
-                        if (getUUID != null) {
-                            getUUID.setAccessible(true);
-                            uuid = (String) getUUID.invoke(methodHookParam);
-                        } else {
-                            XposedBridge.log("[[[AntiAntiDefraud]]] Warning: getUUID method not found.");
-                        }
-                    }else{
-                        XposedBridge.log("[[[AntiAntiDefraud]]] Warning: uuidHelper class not found.");
-                    }
-
-                    JSONObject jSONObject = new JSONObject();
-                    jSONObject.put("timestamp", String.valueOf(System.currentTimeMillis() / 1000));
-                    jSONObject.put("os", MIUI_VERSION);
-                    jSONObject.put("biz_id", "virus_scan");
-                    jSONObject.put("uuid", uuid);
-
-                    JSONArray jSONArray = new JSONArray();
-                    for (int i2 = 0; i2 < list.size(); i2++) {
-                        JSONObject jSONObject2 = new JSONObject();
-
-                        String pkgName = null;
-                        String version = null;
-                        String sign = null;
-                        String appName = null;
-
-                        Object antiDefraudAppInfo = (Object) list.get(i2);
-
-                        Field[] fields = antiDefraudAppInfo.getClass().getDeclaredFields();
-                        for (Field filed : fields) {
-                            filed.setAccessible(true);
-                            if (filed.getName().equals("pkgName")) {
-                                pkgName = (String) filed.get(antiDefraudAppInfo);
-                            } else if (filed.getName().equals("version")) {
-                                version = (String) filed.get(antiDefraudAppInfo);
-                            } else if (filed.getName().equals("sign")) {
-                                sign = (String) filed.get(antiDefraudAppInfo);
-                            } else if (filed.getName().equals("appName")) {
-                                appName = (String) filed.get(antiDefraudAppInfo);
-                            }
-                        }
-
-                        jSONObject2.put("pkg", pkgName);
-                        jSONObject2.put("version", version);
-                        jSONObject2.put("signature", sign);
-                        jSONObject2.put("appname", appName);
-
-                        jSONArray.put(jSONObject2);
-                    }
-                    jSONObject.put("content", jSONArray);
-
-                    XposedBridge.log("[[[AntiAntiDefraud]]] Info: Intercept=" + jSONObject.toString());
-
+                protected void beforeHookedMethod(MethodHookParam methodHookParam) {
+                    XposedBridge.log("[[[AntiAntiDefraud]]] Rejected getAllUnSystemAppsStatus action.");
                     methodHookParam.setResult(null);
+                }
+            });
+
+            XposedBridge.hookMethod(getAllDetectUnsafeApps, new XC_MethodHook() {
+                @Override
+                protected void beforeHookedMethod(MethodHookParam methodHookParam) {
+                    XposedBridge.log("[[[AntiAntiDefraud]]] Rejected getAllDetectUnsafeApps action.");
+                    methodHookParam.setResult(new String[0]);
+                }
+            });
+
+            XposedBridge.hookMethod(getUnSystemAppList, new XC_MethodHook() {
+                @Override
+                protected void beforeHookedMethod(MethodHookParam methodHookParam) {
+                    XposedBridge.log("[[[AntiAntiDefraud]]] Rejected getUnSystemAppList action.");
+                    methodHookParam.setResult(new ArrayList<>());
                 }
             });
         }
